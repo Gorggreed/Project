6b07a97581b7f916dac260dba578c616ed7bf9be
Gorggreed <157367208+Gorggreed@users.noreply.github.com>
2024-12-19 10:58:31 +0000

    Add files via upload

---
+++ webDevAccessibilityLab/website code/styles/image-changer.js
@@ -0,0 +1,6 @@
+function update_text_colour()
+    {
+     const earth_object = document.getElementById("text_update");
+     earth_object.style.color = "red";
+     earth_object.style.fontSize = "30px";
+    }
